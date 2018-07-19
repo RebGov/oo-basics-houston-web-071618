f023d0824620bac538103f6d23502bfa2da8abe2
RebGov <becci.govert@gmail.com>
2018-07-18 21:36:31 -0500

    finished lesson

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,43 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
+end
 
+=begin
+  def title
+      @title
+  end
+=end
+=begin
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+=end
+=begin
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+=end
+=begin
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+=end
